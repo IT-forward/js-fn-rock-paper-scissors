6dc881abf1803d2b21b6c32de384045071b26a33
Otabekkadirov <ganijanovich94@gmail.com>
2020-12-22 12:36:31 +0500

    task 2 in process

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 export const getUserChoice = (userInput) => {
   // TODO
   const input = userInput.trim().toLowerCase();
-  return input;
+  if (input === 'rock') {
+    return input;
+  } else if (input === 'scissors') {
+    return input;
+  } else if (input === 'paper') {
+    return input;
+  } else {
+    console.log('ERROR MESSAGE');
+  }
 };
 
 export const getComputerChoice = () => {
   // TODO
+  const randomIndex = Math.floor(Math.random() * 3);
+  const options = ['rock', 'scissors', 'paper'];
+  return options[randomIndex];
 };
 
 export const determineWinner = (userChoice, computerChoice) => {
