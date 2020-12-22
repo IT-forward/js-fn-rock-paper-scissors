11d16fefc88ad6ea11366465663178cb08aeba34
Otabekkadirov <ganijanovich94@gmail.com>
2020-12-22 10:41:59 +0500

    task 1 again

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 export const getUserChoice = (userInput) => {
   // TODO
   let input = userInput.trim().toLowerCase();
-  if (input === 'rock' || 'paper' || 'scissors') {
+  if (input === 'rock') {
+    return input;
+  } else if (input === 'paper') {
+    return input;
+  } else if (input === 'scissors') {
     return input;
   } else {
     return 'ERROR MESSAGE';
