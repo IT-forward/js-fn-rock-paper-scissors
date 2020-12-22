ccb7f8651b8249554af91a90c95fd2d40da10c3a
Eldorbek7147 <eldor7147@gmail.com>
2020-12-22 20:05:19 +0500

    rock-paper-scissors-updated

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 export const getUserChoice = (userInput) => {
-  // TODO
+  // bo'sh joylarni o'chiramiz va kichik harfga o'tkazamiz.
+  let input = userInput.trim().toLowerCase();
+  // shartlarni birin-ketinlik bilan tekshiramiz
+  if (input === 'rock') {
+    return input;
+  } else if(input === 'paper') {
+    return input;
+  } else if(input === 'scissors') {
+    return input;
+  } else if(input === 'bomb') {
+    return input;
+  }
+  // shartlarni birortasi qanoatlantirmasa, console da Error_message yozuvi paydo bo'ladi
+   else {
+    console.log('Error_message');
+  }
 };
 
 export const getComputerChoice = () => {
-  // TODO
+  // 0 va 3 sonlar orasidan tasodifiy butun sonni qaytaramiz
+  let randomIndex = Math.floor(Math.random() * 3);
+  // Massiv elementlarini kiritib olamiz
+  let element = ['rock','paper','scissors'];
+  // Massivning indexlarini qaytaramiz
+  return element[randomIndex]; 
 };
 
 export const determineWinner = (userChoice, computerChoice) => {
-  // TODO
+  // o'yinchi va kompyuter qiymatlari teng bo'lgandagi shart
+  if (userChoice === computerChoice)  return 'tie';  
+  // switch orqali bularni taqqoslab chiqamiz
+  switch(userChoice) {
+   case 'rock': 
+   if (computerChoice === 'paper') return 'computer won';
+   break;
+   case 'paper': 
+   if (computerChoice === 'scissors') return 'computer won'; 
+   break;
+   case 'scissors':
+   if (computerChoice === 'rock') return 'computer won'; 
+   break;
+  }
+  return 'user won';
 };
 
 const playGame = () => {
-  // TODO
+  let userChoice = getUserChoice('paper');
+  let computerChoice = getComputerChoice();
+  console.log(userChoice,computerChoice);
+  console.log(determineWinner(userChoice,computerChoice));
 };
 
 export default playGame;
+playGame();
