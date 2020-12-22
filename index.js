ef0a947ad4dd9d450ef4d2da53b9aac7c8c00cfa
hamidulla-boltaev <hamidulloboltayev@gmail.com>
2020-12-22 21:11:48 +0400

    task is done

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 export const getUserChoice = (userInput) => {
-  return userInput;
+  let input = userInput.trim().toLowerCase();
+  if (input === "rock") {
+    return input;
+  } else if (input === "paper") {
+    return input;
+  } else if (input === "scissors") {
+    return input;
+  } else if (input === 'bomb'){
+    return input;
+  } else {
+    console.log("Incorrect choice")
+  }
+  
 };
 
 export const getComputerChoice = () => {
-  // TODO
+  let input2 = Math.floor(Math.random() * 3);
+  let randomChoice = ["rock", "paper", "scissors"];
+  return randomChoice[input2];
 };
 
 export const determineWinner = (userChoice, computerChoice) => {
-  // TODO
+  if (userChoice === computerChoice) {
+    return 'tie';
+  } else if (userChoice === "paper" && computerChoice === "rock") {
+    return 'user won';
+  } else if (userChoice === "scissors" && computerChoice === "paper") {
+    return 'user won';
+  } else if (userChoice === "rock" && computerChoice === "scissors") {
+    return 'user won';
+  } else if (userChoice === 'bomb') {
+    return 'user won'
+  } else {
+    return 'computer won';
+  }
 };
 
 const playGame = () => {
-  // TODO
+  let userChoice = getUserChoice('paper');
+  let computerChoice = getComputerChoice();
+  console.log(userChoice, computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
 };
 
 export default playGame;
+
+playGame();
