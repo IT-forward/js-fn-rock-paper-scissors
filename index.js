e2826a12e5c9121243456177685a882632686d33
Bahriddin Abdiev <bahriddin.abdiev@gmail.com>
2020-12-23 20:09:07 +1100

    Revert "Last solution"

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
 export const getUserChoice = (userInput) => {
-  let input = userInput.trim().toLowerCase();
-
-  if (input === 'paper') {
-    return input;
-  } else if (input === 'rock') {
-    return input;
-  } else if (input === 'scissors') {
-    return input;
-  } else if (input === 'bomb') {
-    return input;
-  } else {
-    console.log('Error Message');
-  }
+  // TODO
 };
 
 export const getComputerChoice = () => {
-  let choiceNums = Math.floor(Math.random() * 3);
-  let indexs = ['rock', 'paper', 'scissors'];
-
-  return indexs[choiceNums];
+  // TODO
 };
 
 export const determineWinner = (userChoice, computerChoice) => {
-
-  if (userChoice === computerChoice) {
-    return 'tie'
-  } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-    return 'user won';
-  } else if (userChoice === 'paper' && computerChoice === 'rock') {
-    return 'user won';
-  } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-    return 'user won'
-  } else if (userChoice === 'bomb') {
-    return 'user won'
-  } else {
-    return 'computer won';
-  }
-
+  // TODO
 };
 
 const playGame = () => {
-  let userChoice = getComputerChoice('scissors');
-  let computerChoice = getComputerChoice();
-  determineWinner(userChoice, computerChoice);
-  console.log(determineWinner(userChoice, computerChoice));
+  // TODO
 };
 
 export default playGame;
-playGame();
