b14e0a751c801afe48e7b5566d85cdf38c706a06
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-12-24 18:14:27 +1100

    game is done

--- index.js
+++ index.js
@@ -1,18 +1,71 @@
 export const getUserChoice = (userInput) => {
-  //TODO
-  return userInput.trim().toLowerCase();
+  const cleanInput = userInput.trim().toLowerCase();
+
+  if (
+    cleanInput === "rock" ||
+    cleanInput === "paper" ||
+    cleanInput === "scissors" ||
+    cleanInput === "bomb"
+  ) {
+    return cleanInput;
+  } else {
+    console.log("Input is invalid");
+  }
 };
 
 export const getComputerChoice = () => {
-  // TODO
+  const options = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+
+  return options[randomIndex];
 };
 
 export const determineWinner = (userChoice, computerChoice) => {
-  // TODO
+  if (userChoice === computerChoice) {
+    return "tie";
+  }
+
+  if (userChoice === "bomb") {
+    return "user won";
+  }
+
+  switch (userChoice) {
+    case "rock":
+      if (computerChoice === "paper") {
+        return "computer won";
+      } else {
+        return "user won";
+      }
+      break;
+
+    case "paper":
+      if (computerChoice === "scissors") {
+        return "computer won";
+      } else {
+        return "user won";
+      }
+      break;
+
+    case "scissors":
+      if (computerChoice === "rock") {
+        return "computer won";
+      } else {
+        return "user won";
+      }
+      break;
+  }
 };
 
 const playGame = () => {
-  // TODO
+  const userChoice = getUserChoice("bomb");
+
+  const computerChoice = getComputerChoice();
+
+  console.log("user choice: " + userChoice);
+  console.log("computer choice: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
 };
+
+playGame();
 
 export default playGame;
